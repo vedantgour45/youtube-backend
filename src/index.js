9f4ea0ea5d2f815d8cfdb71287c4e16fde546135
Vedant Gour <vedantgour45@gmail.com>
2026-03-10 08:39:21 +0530

    wip: added utils code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
+
+const port = process.env.PORT || 8000;
 
 dotenv.config();
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+      console.log('Error: ', error);
+      throw error;
+    });
+
+    app.listen(port, () => {
+      console.log(`Server running at port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MongoDB connection failed ', err);
+  });
